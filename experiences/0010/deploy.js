fbf868ebd711cbf0617bc27bff0f551d2e1047d3
Justal Kevin <justal.kevin@gmail.com>
2023-12-03 18:04:58 +0100

    [FIX] Fixing using wsl2

--- experiences/0010/deploy.js
+++ experiences/0010/deploy.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
-    "http://192.168.0.25:7545"
+    "http://127.0.0.1:7545"
   );
   const wallet = new ethers.Wallet(
-    "0x49644116b78d8901dbc2395b8b841c282e8fc1aff1fe397aa85939e413bc2669",
+    "0x4760ba7cd665cbae197b8fab3386aeee15c4b70dfc9e1f7179a776044ed43c37",
     provider
   );
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
